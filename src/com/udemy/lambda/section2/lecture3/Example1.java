89ba8321aa27a6553821f1f3b3e57af5aa2739e1
angelmlnova <amartinez@novasolutionsystems.com>
2019-03-04 18:22:24 -0600

    Se agregan ejemplos de Udemy

---
+++ src/com/udemy/lambda/section2/lecture3/Example1.java
@@ -0,0 +1,32 @@
+package com.udemy.lambda.section2.lecture3;
+
+import java.util.stream.IntStream;
+
+public class Example1 {
+	
+	/*
+	 * Example 1
+	 * Sum of 100 numbers from 0 to 100 
+	 */
+	
+	public static void main(String[] args) {
+
+		// Imperative - how style of programming
+		
+		int sum = 0;
+		for(int i=0; i<=100; i++) {
+			sum+=i;
+		}
+		
+		System.out.println("Sum using Imperative Approach: " + sum);
+		
+		
+		// Declaritive style of programming - what style of programming
+		
+		int sum1 = IntStream.rangeClosed(0, 100).parallel().sum();
+		
+		System.out.println("Sum using Declarative Approach: " + sum1);
+			
+	}
+
+}
---
+++ src/com/udemy/lambda/section2/lecture3/Example2.java
@@ -0,0 +1,14 @@
+package com.udemy.lambda.section2.lecture3;
+
+public class Example2 {
+	
+	/*
+	 * Example 2
+	 * Removing duplicates from a list of integers
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
